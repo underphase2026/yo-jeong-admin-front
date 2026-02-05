a10afa5b00909741c8784756b7a05805e63144fb
박찬민 <able3104@gmail.com>
2026-02-05 17:00:36 +0900

    [Edit] footer 연결 끊음 (주석처리)

--- src/components/layout/defaultLayout.tsx
+++ src/components/layout/defaultLayout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-import Footer from "./footer";
+// import Footer from "./footer";
 
 const DefaultLayout = () => {
   return (
     <div className="w-dvw min-h-dvh bg-blue-tertiary">
       <div className="mx-auto max-w-[75rem] min-w-[44rem] min-h-dvh">
         <Outlet />
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </div>
   );
